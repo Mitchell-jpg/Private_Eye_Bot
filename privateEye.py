adbd359a8da831759aae13c71658a6269bd4ebaf
Mitchell Johnson <mjohnson0671@kctcs.edu>
2024-03-23 03:00:17 -0400

    Added ability to search user comments by keyword.

--- privateEye.py
+++ privateEye.py
@@ -13,12 +13,55 @@ class PrivateEye:
         # Owner Username
         self.owner_username = ""
 
-    def _check_user_comments(self, username, keyword=""):
-        """
-        Check Defined users comments
+    def format_reply(self, message, collected_comments):
+        """ Format response from collected comments"""
         
-        work in progress, keywords will come in th futrue.
-        """
+        list_to_string = ""
+        num_of_comments = 0
+
+        #Process comments found
+        
+        for comment in collected_comments:
+
+            # Check for errors
+            if comment == "no comments matching keyword was found.":
+                message.reply("no comments matching keyword was found.")
+            elif comment == "something went wrong":
+                message.reply("something went wrong")
+            else:
+
+                # Seperate comments into their own list
+                for element in comment :
+                    print(comment)
+                    num_of_comments += 1
+                    list_to_string += element + "\n\n"
+        
+        text = f"#Found {num_of_comments}Comment(s):\n\n {list_to_string}"
+        return self.convert_to_markdown(text)
+
+    def convert_to_markdown(self, text):
+        """ Converts string into markdown"""
+        markdown = ''
+        lines = text.split('\n')
+        
+        for line in lines:
+            if line.startswith('#'):
+                level = line.count('#')
+                markdown += f"{'#' * level} {line[level:].strip()}\n"
+            elif line.startswith('* '):
+                markdown += f"- {line[2:]}\n"
+            elif line.startswith('1. '):
+                markdown += f"1. {line[3:]}\n"
+            elif line.startswith('> '):
+                markdown += f"> {line[2:]}\n"
+            else:
+                markdown += line + '\n'   
+        return markdown
+    
+    def check_user_comments(self, username, keywords=""):
+        """Check user comments for keywords.  If no keywords, return last 5 comments"""
+
+        collected_comments = []
 
         # Create instance of comments
         comments = self.reddit.redditor(username).comments.new
@@ -26,15 +69,8 @@ class PrivateEye:
         # Search for most recent comments; display last 5 if found.
         print("Searching for recent comments...\n")
 
-        collected_comments = []
-
         # If no searchable keywords, return last 5 comments.
-        
-        if keyword == "":
-            
-
-            print("No keywords..  Printing last 5 comments\n")
-
+        if keywords == "":
             try:
                 for comment in comments(limit=5):
                     
@@ -43,51 +79,54 @@ class PrivateEye:
                     # Convert UTC to Local
                     time_obj = time.localtime(comment.created_utc)
                     local_time = time.strftime('%Y-%m-%d %H:%M:%S', time_obj)
-                    
+                        
                     # return comments.
-                    extracted_comment.append( 
-                        f"Subreddit name : r/{comment.subreddit.display_name} |"              +
-                        comment.body                                                        +
-                        " | "                                                                +
-                        f"Karma: {comment.score}  |  Posted at: {local_time} "             +                                        
-                        f"Link: https://reddit.com{comment.permalink} "  
+                    # return comments, ready for markdown.
+                    extracted_comment.append ( 
+                        f"**Subreddit name:** r/{comment.subreddit.display_name} \n\n"        +
+                        "---\n\n"                                                                 +
+                        comment.body + "\n\n"                                                 +
+                        "---\n\n"                                                                 +
+                        f"Karma: {comment.score}  |  Posted at: {local_time}\n\n"             +                                        
+                        f"[Link_to_comment](https://reddit.com{comment.permalink})\n\n"  
                     )
                     collected_comments.append(extracted_comment)
             except:
                 return ""
-            
+                
             return collected_comments
                 
-        # Search for keywords, return matching comments.
+        # Search for keywords, return matching comments (maximum 30)
         else:
             try:
-                for comment in comments:
-                    if keyword in comment:
-                        extracted_comment = []
+                for comment in comments(limit=30):
+                    for keyword in keywords:
+                        if keyword in comment.body:
+                            extracted_comment = []
 
-                        # Convert UTC to Local
-                        time_obj = time.localtime(comment.created_utc)
-                        local_time = time.strftime('%Y-%m-%d %H:%M:%S', time_obj)
-                        
-                        # Store in temporary list.
-                        extracted_comment.append(f"Subreddit name : {comment.subreddit.display_name} " )
-                        extracted_comment.append(comment.body + " ")                                                        
-                        extracted_comment.append("")                                                               
-                        extracted_comment.append(f"Karma: {comment.score}  |  Posted at: {local_time} ")                         
-                        extracted_comment.append(f"Link: https://reddit.com{comment.permalink} ")
-
-                        collected_comments.append(extracted_comment)
+                            # Convert UTC to Local
+                            time_obj = time.localtime(comment.created_utc)
+                            local_time = time.strftime('%Y-%m-%d %H:%M:%S', time_obj)
+                            
+                            # Store in temporary list.
+                            extracted_comment.append ( 
+                                f"**Subreddit name:** r/{comment.subreddit.display_name} \n\n"        +
+                                "---\n\n"                                                                 +
+                                comment.body + "\n\n"                                                 +
+                                "---\n\n"                                                                 +
+                                f"Karma: {comment.score}  |  Posted at: {local_time}\n\n"             +                                        
+                                f"[Link_to_comment](https://reddit.com{comment.permalink})\n\n"
+                            )  
+                            collected_comments.append(extracted_comment)
             except:
                 return ""
             
-
             if collected_comments == []:
                 return "no comments matching keyword was found."
             else:
                 return collected_comments
-
-    
-    def _get_user_info(self, username):
+ 
+    def get_user_info(self, username):
 
         # Create instance of user
         self.user = self.reddit.redditor(username)
@@ -96,61 +135,73 @@ class PrivateEye:
         try:
             time_obj = time.localtime(self.user.created_utc)
             local_time = time.strftime('%Y-%m-%d %H:%M:%S', time_obj)
-        
-        
+              
             # Information on user
-            basic_info = (
-            fr"Basic info for user: {self.user}\  " +
-            fr"Karma: {self.user.comment_karma}\  " +                                     
-            fr"Link to Profile pic: {self.user.icon_img}\  " +                             
-            fr"Reddit ID: {self.user.id}\  " +                                              
-            fr"Account Age: {local_time}\  " +           
-            fr"Verifed Email? : {self.user.has_verified_email}\  " +                        
-            fr"Reddit employee? {str(self.user.is_employee)}\  " +                          
-            fr"Moderator? : {str(self.user.is_mod)}\  " +                                 
-            fr"Reddit Premium?: {str(self.user.is_gold)}&nbsp;")
+            text = (f"##{self.user}\n\n> Karma: {self.user.comment_karma}\n\n> [Profile_pic]({self.user.icon_img})\n\n>" +                               
+            f" Reddit ID: {self.user.id}\n\n> Account made @ {local_time}\n\n> Verifed Email?: {self.user.has_verified_email}\n\n" +             
+            f"> Reddit employee? {str(self.user.is_employee)}\n\n> Moderator?: {str(self.user.is_mod)}\n\n> Reddit Premium?: {str(self.user.is_gold)}\n\n")
         except:
             return f"{username} not found."
         
-        return basic_info
+        return self.convert_to_markdown(text)
 
-    def _search_for_keywords(self, message):
-            """Search for keywords in messages"""
-
-            
-            # Allow bot owner to shutdown remotley; Block others who try.
-            if message.subject == "!shutdown": 
-                if message.author == self.settings.owner:
-                    bot_active = False
-                else:
-                    print(f"Blocked user {message.author} for trying to shutdown bot.")
-                    self.message.mark_read()
-                    self.message.block()
-            
-            elif message.subject == "!search":
-
-                target_username = message.body
-                collected_comments = self._check_user_comments(target_username)
-                
-                formatted_reply = ""
-                
-                # Process comments
-                for comment in collected_comments:
-                    formatted_reply += fr"{comment}\  "
-            
-                
-                message.reply(fr"{self._get_user_info(target_username)}\  {formatted_reply}")
-                message.mark_read()
-                print(f"Infromation on {target_username} provided to {message.author}.\nResuming inbox monitoring")
-                
-
+    def search_for_commands(self, message):
+        """Search for keywords in messages"""
+           
+        # Allow bot owner to shutdown remotley; Block others who try.
+        if message.subject == "!shutdown": 
+            if message.author == self.settings.owner:
+                bot_active = False
             else:
-                message.reply("Try again.")
-                print(f"sent try again message to {message.author}")
-                message.mark_read()
+                print(f"Blocked user {message.author} for trying to shutdown bot.")
+                self.message.mark_read()
+                self.message.block()
+            
+        elif message.subject == "!search" or "Re: !search":
+
+            body = message.body
+                
+            # Check if delimiter used, if not format username and search last 5 comments.
+            if "|" in body:
+
+                sepmsg = body.split("|")
+
+                # If the delimiter splits in anything other than two the format must be wrong.
+                if len(sepmsg) != 2:
+                    message.reply("Wrong format used.  Seperate username and keywords using '|' and seperate keywords by ','")
+                else:
+
+                    username = sepmsg[0].strip(" ").lower()
+                        
+                    # Check if there are multiple keywords; format and check for comments.
+                    if "," in sepmsg[1]:
+                        keywords = sepmsg[1].split(",")
+                        for i in range(len(keywords)):
+                            keywords[i] = keywords[i].strip().lower()
+                            
+                    else:
+                        keywords = sepmsg[1]
+                        for keyword in keywords:
+                            keyword.strip(" ").lower()
+
+                    collected_comments = self.check_user_comments(username, keywords)
+                    formatted_reply = self.format_reply(message, collected_comments)
+            else:
+                    username = body.strip(" ").lower()
+                    collected_comments = self.check_user_comments(username)
+                    formatted_reply = self.format_reply(message, collected_comments)    
+                
+            message.reply(f"{self.get_user_info(username)} \n {formatted_reply}")
+            message.mark_read()
+            print(f"Infromation on {username} provided to {message.author}.\nResuming inbox monitoring\n")
+                
+        else:
+            message.reply("Try again.")
+            print(f"sent try again message to {message.author}")
+            message.mark_read()
 
         
-    def _check_messages(self):
+    def check_messages(self):
         """Monitor bot's inbox for keywords"""
         
         inbox = self.reddit.inbox.unread()
@@ -165,9 +216,7 @@ class PrivateEye:
                   message.body
                   )
             
-            self._search_for_keywords(message)
-                
-           
+            self.search_for_commands(message)
 
 
     def run_bot(self):
@@ -177,21 +226,12 @@ class PrivateEye:
         bot_active=True
 
         self.owner_username = input("Enter the bot owner's username: ")
-        
-
         print("monitoring for messages")
 
         while bot_active:
            
-            self._check_messages()
+            self.check_messages()
             time.sleep(5)
-
-
-        
-            
-                    
-                    
-
 
 
 if __name__ == "__main__":
