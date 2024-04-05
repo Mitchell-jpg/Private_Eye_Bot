ad4f272c1a5de73c96c53e765f6f781ff5b05958
Mitchell Johnson <mjohnson0671@kctcs.edu>
2024-04-05 12:22:57 -0400

    refacotring for greater re-usablility.

--- private_eye.py
+++ private_eye.py
@@ -1,17 +1,17 @@
 import sys
-from functools import wraps
 import praw
 import time
 import requests
-from prawcore.exceptions import NotFound, TooManyRequests, InvalidToken, ResponseException
 from inbox import Inbox
 from reddit import RedditUserData
+from functools import wraps
+from prawcore.exceptions import NotFound, TooManyRequests, InvalidToken, ResponseException
 
 
 class PrivateEye:
     """Overall Class to manage bot"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize bot"""
 
         # Create reddit instance
@@ -25,11 +25,14 @@ class PrivateEye:
     def connectivity_check(func: any) -> any:
         """ 
         Checks for internet connectivity.
+
+        Prevent's actions that require internet connectivity from being performed.
+
+        This will still allow the bot to initialize.
         
         """
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            
+        def wrapper(*args, **kwargs) -> func:
             """ 
             Attempt to make a GET request 3 times.
             Continues if no connection can be made
@@ -42,7 +45,7 @@ class PrivateEye:
             
                 try:
                     requests.get("Http://google.com", timeout=3)
-                    func(*args, **kwargs)
+                    return func(*args, **kwargs)
                 except requests.ConnectionError:
                     attempts += 1
                     print(f"attempting to establish connection..  Attempt {attempts} of {max_attempts}.")
@@ -55,6 +58,22 @@ class PrivateEye:
             print("\nUnable to establish internet connection. Private Eye cannot process requests until internet connectivity is re-established.\n")
             func(pi)
             
+        return wrapper
+    
+    def record_results(func: any) -> None:
+        """ 
+        Takes results and appends them to results.txt 
+        """
+        def wrapper(*args, **kwargs) -> func:
+
+            results = func(*args, **kwargs)
+        
+            today = time.localtime()
+            print("This information has been recorded in the results.txt file.")
+            with open("results.txt", "a") as file:
+                file.write(results + f"Date retrieved: {today.tm_mon}/{today.tm_mday}/{today.tm_year} @ {today.tm_hour}:{today.tm_min}\n")
+                file.close()
+                
         return wrapper
         
 
@@ -91,20 +110,21 @@ class PrivateEye:
             # Displays help message
             if user_input == "help" or user_input == "h":
                 print("The available commands are: 'start bot', 'search', and 'help'.  Type 'quit' to end the program.")
+            # Ends Program
             elif user_input == "quit" or user_input == "q":
                 print("goodbye.")
                 sys.exit()
             else:
                 try:
+                    # Attempts to locate commands that require API calls.
                     self._check_user_input(user_input)
                 except ResponseException as e:
                     return print(f"{e}, check Praw.ini file for errors.")
             
             # if user inputs, 'Start bot': Initialize ability to recieve commands via reddit PM  
-            bot_active = self.bot_active
-            if bot_active == True:
+            if self.bot_active == True:
                 print("\nBot activated sucessfully, monitoring for messages.  Press CTRL + C to stop bot locally.")
-            while bot_active == True:
+            while self.bot_active == True:
                 try:
                     self.reddit_inbox.check_messages()    
                     time.sleep(10)
@@ -121,6 +141,8 @@ class PrivateEye:
 
         searches for strings "start bot", and "search".
 
+        Requires Internet conectivity.
+
         """
         
 
@@ -130,78 +152,73 @@ class PrivateEye:
                 # Prompts user to setup username that is allowed to send !shutdown message.  Begins while loop in run_bot()
                 self.reddit_inbox.setup_bot_owner()
                 self.bot_active = True
-                
-            
 
         # Search for user, optionally search user comments.
         elif user_input == "search" or user_input == "s":
             try:    
-                # Gathers target username from user.
-                while True:
-                    username = input("\nPlease enter a username to search, enter 'go back' to cancel: ")
-                    username = username.lower().strip(" ")
-
-                    if username == "go back":
-                        print("\ncancelling search..\n")
-                        break
-
-                    # Confirm the target exists, else reprompt user.
-                    elif not self.check_user_exists(username):
-                        continue
-                    break
-         
-                # Gathers if the user wants to search comments. If so, prompts for keywords.
-                while True:
-                    # Gather user choice
-                    search_comments = input(f"\nuser {username} found. Search their comments? yes or no: ")
-
-                    if search_comments.lower() == "go back":
-                        print("\nCancelling search..\n")
-                        break
-
-                    # If no comments are searched, return only basic user info.
-                    if search_comments.lower() == "no":
-                        results = self.reddit_user_data.get_user_info(username)
-                        print(results)
-                        self.record_results(results)
-                        break
-                    # If the input is not 'yes', or 'no'. reprompt user for choice.
-                    elif search_comments.lower() != "no" and search_comments.lower() != "yes":
-                        print("that is not a valid command. Please enter 'yes', or 'no'")
-                        continue
-                    # Obtain keyword or keywords to search by, turn into a list, search the users comments for the keywords.
-                    keyword_input = input("\nEnter Keyword(s) seperated by ','.  Press enter for none: ")
-                    keywords = self._format_keywords(keyword_input)
-                    results = self._find_results(username, keywords)
-                    self.record_results(results)
-                    break
+                self._perform_search_command()
 
             except ResponseException as e:
                 return print(f"{e}, check Praw.ini file for errors.")
+                
+    def _perform_search_command(self) -> str:
+        """ 
+        Performs search based on user input of username and keywords
 
-    def check_user_exists(self, username: str) -> bool:
+        returns results. 
         """
-        Checks if username is tied to a user.
-        Returns T or F
-
-        """
+                
+        # Gathers valid target username from user.
+        username = self._gather_username_to_search()
         
-        #Check if user exists.
-        print(f"\nsearching for {username}..")
-        username_check = self.reddit_user_data.get_user_info(username)
-                        
-        # Reprompt is user does not exist.
-        if not username_check:
-            return False  
-        return True
+        # Gathers if the user wants to search comments. If so, prompts for keywords.
+        if not self._ask_to_check_comments(username):
+            results = self._find_reddit_results(username, None, False)
+            return results
+
+        else:        
+            #If user wants to search for comments, gather keywords.
+            keyword_input = input("\nEnter Keyword(s) seperated by ','.  Press enter for none: ")
+            keywords = self._format_keywords(keyword_input)
+            results = self._find_reddit_results(username, keywords, True)
+            return results
+
+
+    def _gather_username_to_search(self) -> str:
+        """ 
+        Gathers input from user, checks for validity, returns formatted string 
+        """
+
+        while True:
+                username = input("\nPlease enter a username to search: ")
+                username = username.lower()
+                
+                #Check if user exists.
+                if self.reddit_user_data.check_user_exists(username):
+                    return username
+                
+                print(f"\nUsername {username} could not be found.  Check for spelling mistakes.\n")
+                continue
+        
+    def _ask_to_check_comments(self, username: str) -> bool:
+
+        while True:
+            # Gather user choice
+            search_comments = input(f"\nuser {username} found. Search their comments? yes or no: ")
+
+            if search_comments.lower() == "no":
+                return False
+            elif search_comments.lower() != "no" and search_comments.lower() != "yes":
+                print("that is not a valid command. Please enter 'yes', or 'no'")
+                continue
+            return True
+
   
     def _format_keywords(self, keyword: str = None) -> list:
         """
         Formats supplied keywords into a list
 
-        """
-
-        
+        """ 
         if not keyword:
                 # if user passed no keywords, return null list.
                 keywords = None
@@ -222,54 +239,29 @@ class PrivateEye:
                     keyword.strip(" ").lower()
                 return keywords
 
-    
-    def _find_results(self, username: str, keylist: list = None) -> str:
+    @record_results
+    def _find_reddit_results(self, username: str, keylist: list = None, check_comments: bool = False) -> str:
         """
-        Takes username and keywor list and queries reddit user data for results.
+        Takes username and keyword list and queries reddit user data for results.
 
-    
         """
+        if not check_comments:
+            results = f"{self.reddit_user_data.get_user_info(username)}\n"
+            print(results)
+            return results
 
         if keylist is None:
             # If no keywords return last 5 comments.
-            collected_comments = self.reddit_user_data._check_user_comments(username)
-
-            if collected_comments == []:
-                # If no comment's return user info
-                results = f"{self.reddit_user_data.get_user_info(username)}\n"
-            else:
-                # if comments present, format them and return user info and comments
-                formatted_comments = self.reddit_user_data.format_comments(collected_comments)
-                results = f"{self.reddit_user_data.get_user_info(username)}\n {formatted_comments}"
+            formatted_comments = self.reddit_user_data.check_user_comments(username)
+            results = f"{self.reddit_user_data.get_user_info(username)}\n {formatted_comments}"
         else:
             # Keywords are present, search comments for mentions of keywords.
-            collected_comments = self.reddit_user_data._check_user_comments(username, keylist)
+            formatted_comments = self.reddit_user_data.check_user_comments(username, keylist)
 
-            if collected_comments == []:
-                # No comments matching keywords.  Return user info.
-                results = f"{self.reddit_user_data.get_user_info(username)}\n"
-            else:
-                # Keywords present.  Return user info and comments with keywords highlighted.
-                formatted_comments = self.reddit_user_data.format_comments(collected_comments)
-                results = f"{self.reddit_user_data.get_user_info(username)}\n {formatted_comments}"
+            results = f"{self.reddit_user_data.get_user_info(username)}\n {formatted_comments}"
         
         print(results)
-        return results
-    
-    def record_results(self, results):
-        """ 
-        Takes results and appends them to results.txt 
-        
-        kwags:
-        
-        results --> str
-        """
-        
-        today = time.localtime()
-        print("This information has been recorded in the results.txt file.")
-        with open("results.txt", "a") as file:
-            file.write(results + f"Date retrieved: {today.tm_mon}/{today.tm_mday}/{today.tm_year} @ {today.tm_hour}:{today.tm_min}\n")
-            file.close()
+        return results 
     
 if __name__ == "__main__":
     pi = PrivateEye()
